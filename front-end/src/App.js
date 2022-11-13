1b93393d77b8504dc19db84ecaa8b678a66841be
= <=>
2022-11-13 18:35:35 +0300

    Setting up frontend routes
    - Using react router to handle all the routing.
    - /auth/register or /auth/login will now take you to an actual page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+	return (
+		<>
+			<Routes>
+				<Route path="/auth">
+					<Route path="login" element={<h1>Login Page</h1>}></Route>
+					<Route path="register" element={<h1>Register Page</h1>}></Route>
+				</Route>
+				<Route path="/">
+					<Route path="" element={<h1>Main Page</h1>}></Route>
+				</Route>
+			</Routes>
+		</>
+	);
+};
 
-export default App
+export default App;
