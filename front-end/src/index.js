9a277c302fa1bb61b705c17df78a85c59a66e5a0
Benedict <benedict.arowo@gmail.com>
2022-11-15 23:35:18 +0300

    Moved UserContext to index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import UserContext from "./contexts/UserContext";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<UserContext>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</UserContext>
 	</React.StrictMode>
 );
