8605e5af65d81a69cda4fc940fce61c390611358
Benedict <benedict.arowo@gmail.com>
2022-11-15 10:15:24 +0300

    Added view/hide icon for register form.

--- front-end/src/pages/authentication/register.jsx
+++ front-end/src/pages/authentication/register.jsx
@@ -1,10 +1,12 @@
 import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import { EMAIL_PATTERN, PASSWORD_PATTERN, USERNAME_PATTERN } from '../../config'
 import { fetchReq } from '../../fetchReq'
 
 const Register = () => {
-    let passwordHolder = useRef('')
+    const passwordHolder = useRef('')
+    const [passwordType, setPasswordType] = useState()
     const [userCredentials, setUserCredentials] = useState({
         username: '',
         email: '',
@@ -65,6 +67,7 @@ const Register = () => {
         e.preventDefault()
         const element = passwordHolder.current
         element.type = element.type === 'password' ? 'text' : 'password'
+        setPasswordType(() => element.type) // For deciding on what icon to display
     }
 
     return (
@@ -84,7 +87,17 @@ const Register = () => {
                 <section>
                     <label htmlFor="password">Password</label>
                     <input ref={passwordHolder} type="password" name="password" value={userCredentials.password} onChange={e => handleChange(e.currentTarget.name, e.target.value)} id="password" />
-                    <button onClick={(e) => togglePasswordView(e)} id="showPassword">show password</button>
+                    {passwordType === 'text' ?
+                        <Icon onClick={(e) => togglePasswordView(e)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className='passwordIcon'>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88" />
+                        </Icon>
+                        :
+                        <Icon onClick={(e) => togglePasswordView(e)} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className='passwordIcon'>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </Icon>
+
+                    }
                 </section>
 
                 <button>Register</button>
@@ -94,4 +107,7 @@ const Register = () => {
     )
 }
 
+const Icon = styled.svg`
+    width: 24px;
+`
 export default Register
