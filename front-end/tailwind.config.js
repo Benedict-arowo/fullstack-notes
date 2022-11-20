cf22da006e45aa60f4093c4efb293d0780b088c8
Benedict <benedict.arowo@gmail.com>
2022-11-20 23:45:34 +0300

    Added tailwindcss

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		minHeight: {
+			"3/4": "70%",
+		},
+		extend: {},
+	},
+	plugins: [],
+};
