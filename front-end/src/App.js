03ee321bfeba927090329b40cc6aa1d5408819b8
Benedict <benedict.arowo@gmail.com>
2022-11-20 23:44:57 +0300

    Rerouting

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,26 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Nav from "./components/nav.jsx";
+import Nav from "./components/Nav.jsx";
 import Login from "./pages/authentication/Login.jsx";
 import Register from "./pages/authentication/Register.jsx";
 import "./index.css";
-import CheckAuth from "./contexts/CheckAuth";
+import RedirectAuth from "./contexts/RedirectAuth";
+import UseAuth from "./contexts/UseAuth.js";
+import Index from "./pages/Index.jsx";
 
 const App = () => {
 	return (
 		<>
 			<Routes>
-				<Route path="" element={<CheckAuth />}>
+				<Route path="" element={<RedirectAuth />}>
 					{/* For Authentication */}
 					<Route path="login" element={<Login />}></Route>
-					<Route path="register" element={<Register />}></Route>
+					<Route path="register" element={<Register />} />
 				</Route>
-				<Route path="" element={<Nav />}>
-					<Route index element={<h1>Main Page</h1>}></Route>
+				<Route path="" element={<UseAuth />}>
+					<Route path="" element={<Nav />}>
+						<Route index element={<Index />} />
+					</Route>
 				</Route>
 			</Routes>
 		</>
