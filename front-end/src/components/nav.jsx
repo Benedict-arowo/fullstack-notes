53867a1d7cda28359f0615479c092e13f502797f
Benedict <benedict.arowo@gmail.com>
2022-11-21 23:24:52 +0300

    Made it a container element to allow pages to easily fit in

--- front-end/src/components/nav.jsx
+++ front-end/src/components/nav.jsx
@@ -3,13 +3,15 @@ import { Outlet } from 'react-router-dom'
 
 const Nav = () => {
     return (
-        <>
+        <div className='flex flex-col w-full h-full'>
             <div className='bg-blue-400'>
                 Nav
             </div>
+            <div className='flex flex-row w-full h-full'>
+                <Outlet />
+            </div>
 
-            <Outlet />
-        </>
+        </div>
     )
 }
 
