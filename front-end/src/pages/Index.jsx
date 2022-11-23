2d91056f4456f161be2ee8fa24c02bd00b21daa3
Benedict <benedict.arowo@gmail.com>
2022-11-23 12:28:15 +0300

    Added main content

--- front-end/src/pages/Index.jsx
+++ front-end/src/pages/Index.jsx
@@ -6,7 +6,26 @@ const Index = () => {
     return (
         <>
             <Sidebar />
-            <h1 className='text-3xl font-bold underline'>Howdy</h1>
+            <main className='w-full h-full pt-4 bg-blue-100'>
+                <h1 className='text-3xl font-bold text-blue-800 text-center'>My Notes</h1>
+
+                <section className='mx-8 mt-4'>
+
+                    <input autoComplete='off' autoCorrect='false' type="text" name="Search" id="" className='w-full rounded-full py-2 px-4 hover:drop-shadow-lg active:drop-shadow-lg' placeholder='Search...' />
+
+                    <section className='flex w-full items-top justify-between px-4 text-gray-500 items-center mt-1'>
+                        <p>Currently displaying {2} items.</p>
+                        <select name="sort list" id="sortlist" className='px-2 py-1 rounded-full border outline-none bg-blue-300 text-slate-100'>
+                            <option value="updated" selected disabled>Sort By</option>
+                            <option value="title">Title</option>
+                            <option value="name">Name</option>
+                            <option value="created">Created</option>
+                            <option value="updated">Updated</option>
+                            {/* TODO: Maybe an api call to get all the sort options */}
+                        </select>
+                    </section>
+                </section>
+            </main>
         </>
     )
 }
