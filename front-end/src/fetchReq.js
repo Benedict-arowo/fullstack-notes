39b36f4f6951590e702fcd32692426f590a0f489
Benedict <benedict.arowo@gmail.com>
2022-11-28 12:27:33 +0300

    Changed fetchReq to a context component.

--- front-end/src/fetchReq.js
+++ front-end/src/fetchReq.js
@@ -1,10 +1,32 @@
+import React, { createContext, useContext } from 'react'
+import { useNavigate } from "react-router-dom";
 import { API_URL } from "./config";
 
-export const fetchReq = async ({ url, options }) => {
-	try {
-		const response = await fetch(`${API_URL}${url}`, options);
-		return await response.json();
-	} catch (error) {
-		return error; // TODO: Handle error using error context
-	}
-};
+
+const FetchReq = createContext()
+export const useFetch = () => useContext(FetchReq)
+
+const FetchContext = ({ children }) => {
+	const Navigate = useNavigate()
+	const fetchData = async ({ url, options }) => {
+		try {
+			const response = await fetch(`${API_URL}${url}`, options);
+			if (response.status === 401) {
+				// If user is not authenticated || Invalid credentials
+				localStorage.removeItem('token');
+				return Navigate("../login")
+			}
+			return response;
+		} catch (error) {
+			return error; // TODO: Handle error using error context
+		}
+	};
+
+	return (
+		<FetchReq.Provider value={fetchData}>
+			{children}
+		</FetchReq.Provider>
+	)
+}
+
+export default FetchContext
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import UserContext from "./contexts/UserContext";
 import { BrowserRouter } from "react-router-dom";
 import ThemeContext from "./contexts/ThemeContext";
+import FetchContext from "./fetchReq";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -12,7 +13,9 @@ root.render(
 		<UserContext>
 			<ThemeContext>
 				<BrowserRouter>
-					<App />
+					<FetchContext>
+						<App />
+					</FetchContext>
 				</BrowserRouter>
 			</ThemeContext>
 		</UserContext>
