cc8b5df1dee948f09fca4637b0fb755f9077ab42
Benedict <benedict.arowo@gmail.com>
2022-11-28 09:32:57 +0300

    Added ThemeContext

---
+++ front-end/src/contexts/ThemeContext.js
@@ -0,0 +1,42 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+
+const ThemeProvider = createContext()
+const useThemeProvider = createContext()
+
+export const useTheme = () => useContext(ThemeProvider)
+export const useSetTheme = () => useContext(useThemeProvider)
+
+const ThemeContext = ({ children }) => {
+    const [theme, setTheme] = useState()
+
+    const updateTheme = (mode) => {
+        if (mode === 'dark' || mode === 'light' || !mode) {
+            setTheme(mode)
+        }
+    }
+
+    useEffect(() => {
+        if (!theme) {
+            localStorage.removeItem('theme')
+        }
+        else {
+            localStorage.setItem('theme', theme)
+            if (theme === 'dark') {
+                document.documentElement.classList.add('dark');
+            }
+            else {
+                document.documentElement.classList.remove('dark');
+            }
+        }
+    }, [theme])
+
+    return (
+        <ThemeProvider.Provider value={theme}>
+            <useThemeProvider.Provider value={updateTheme}>
+                {children}
+            </useThemeProvider.Provider>
+        </ThemeProvider.Provider>
+    )
+}
+
+export default ThemeContext
