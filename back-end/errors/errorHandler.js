4df3702c4d9e4fd92b35bd1b8adcd98f67562c57
Benedict <benedict.arowo@gmail.com>
2022-11-29 15:08:28 +0300

    Moved console.log upwards, to check error names before it is used to make decisions.

--- back-end/errors/errorHandler.js
+++ back-end/errors/errorHandler.js
@@ -2,10 +2,10 @@ const { StatusCodes } = require("http-status-codes");
 
 const ErrorHandler = (err, req, res, next) => {
 	// Need to handle error made from customError Class
+	console.log(err.name);
 	if (err.name === "JsonWebTokenError" || err.name === "TokenExpiredError") {
 		return res.status(StatusCodes.UNAUTHORIZED).json({ err: "UNAUTHORIZED" });
 	}
-	console.log(err.name);
 	res.status(StatusCodes.BAD_REQUEST).json({ err: err.message });
 };
 
