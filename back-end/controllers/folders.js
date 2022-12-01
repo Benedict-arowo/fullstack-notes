3900e297bf2250515fd92b0eb98235ba3b333ffc
Benedict <benedict.arowo@gmail.com>
2022-12-01 23:50:43 +0300

    A

--- back-end/controllers/folders.js
+++ back-end/controllers/folders.js
@@ -1,9 +1,19 @@
 const { asyncWrapper } = require("../middlewears");
 const Folders = require("../models/Folders");
+const { StatusCodes } = require("http-status-codes");
 
 const getFolders = asyncWrapper(async (req, res) => {
-	const data = await Folders.find({});
-	res.json(data);
+	const { query } = req.query;
+	const data = Folders.find({});
+	console.log(query);
+
+	if (query) {
+		const pattern = new RegExp(`.*${query}.*`, "i");
+		data.find({ name: pattern });
+	}
+
+	const result = await data;
+	res.json(result).status(StatusCodes.OK);
 });
 
 const newFolder = asyncWrapper(async (req, res) => {
