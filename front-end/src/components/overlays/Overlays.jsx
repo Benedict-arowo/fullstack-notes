1f06c5f9ba75526dc982b3c6ab1d267919d16547
Benedict <benedict.arowo@gmail.com>
2022-12-02 15:35:34 +0300

    Changed Controlled Components to Item, and Folder components respectively.

--- front-end/src/components/overlays/Overlays.jsx
+++ front-end/src/components/overlays/Overlays.jsx
@@ -15,10 +15,22 @@ const Overlays = ({ children }) => {
     const customFetch = useFetch()
     const FolderElement = useRef()
     const ItemElement = useRef()
-    const [ controlledComponent, setControlledComponent ] = useState({
+    const [ folderComponent, setFolderComponent ] = useState({
         folder: '',
         color: '#ffffff'
     })
+    const [ itemComponent, setItemComponent ] = useState({
+        name: '',
+        folder: '',
+        folderId: '',
+        note: '',
+        mode: '',
+    })
+    const modeList = [
+        { value: 'plain', label: 'Plain'},
+        { value: 'markdown', label: 'Markdown'}
+    ]
+
     const [folderList, setFolderList] = useState() 
 
     const showFolder = () => {
@@ -30,8 +42,8 @@ const Overlays = ({ children }) => {
 
     }
 
-    const updateControlledComponent = ({item, newValue}) => {
-        setControlledComponent(prev => {
+    const updateFolderComponent = ({item, newValue}) => {
+        setFolderComponent(prev => {
             return {
                 ...prev,
                 [`${item}`]: newValue
@@ -39,7 +51,40 @@ const Overlays = ({ children }) => {
         })
 
         setTimeout(() => {
-            console.log(controlledComponent)
+            console.log(folderComponent)
+        }, 100)
+    }
+
+    const updateItemComponent = ({item, newValue}) => {
+        if (item === 'folder') {
+            setItemComponent(prev => {
+                return {
+                    ...prev,
+                    folderId: newValue.value,
+                    folder: newValue.label
+                };
+            })
+            return
+        }
+        else if (item === 'mode') {
+            setItemComponent(prev => {
+                return {
+                    ...prev,
+                    mode: newValue.value,
+                };
+            })
+            return
+        }
+
+        setItemComponent(prev => {
+            return {
+                ...prev,
+                [`${item}`]: newValue
+            };
+        })
+
+        setTimeout(() => {
+            console.log(itemComponent)
         }, 100)
     }
 
@@ -67,22 +112,38 @@ const Overlays = ({ children }) => {
         mode === 'folder' ? showFolder() : showItem();
     }
 
+    const newItem = async (event) => {
+        event.preventDefault();
+        // name: '',
+        // folder: '',
+        // folderId: '',
+        // note: '',
+        const response = await customFetch({url: 'notes', options: {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json', 'Authorization': localStorage.getItem('token')},
+            body: JSON.stringify({ title: itemComponent.name, note: itemComponent.note, folderName: itemComponent.folder, folderId: itemComponent.folderId })
+        }})
+
+        const data = await response.json()
+        console.log(data)
+    }
+
     const newFolder = async (event) => {
         event.preventDefault();
         const response = await customFetch({url: 'folders', options: {
             method: 'POST',
             headers: {'Content-Type': 'application/json', 'Authorization': localStorage.getItem('token')},
             body: JSON.stringify({
-                name: controlledComponent.folder,
-                color: controlledComponent.color
+                name: folderComponent.folder,
+                color: folderComponent.color
             })
         }})
 
         const data = await response.json()
 
         FolderElement.current.click() // Simulates a click on the FolderElement element, causing the Overlay to be hidden.
-        updateControlledComponent({newValue: '', item: 'folder'})
-        updateControlledComponent({newValue: '', item: 'color'})
+        updateFolderComponent({newValue: '', item: 'folder'})
+        updateFolderComponent({newValue: '', item: 'color'})
     }
     
     useEffect(() => {
@@ -92,6 +153,7 @@ const Overlays = ({ children }) => {
                 headers: {'Content-Type': 'application/json', 'Authorization': localStorage.getItem('token')}
             }})
             const data = await response.json()
+            console.log(data)
             let folderlist = data.map(item => {
                 return {
                     value: item._id,
@@ -103,7 +165,7 @@ const Overlays = ({ children }) => {
         })()
     }, [])
 
-    console.log(folderList)
+    // console.log(folderList)
 
 
     return (
@@ -111,19 +173,22 @@ const Overlays = ({ children }) => {
             {children}
             <div ref={FolderElement} className='overlay grid place-content-center duration-300' onClick={hideOverlay}>
                 <form action="" onSubmit={newFolder} className='bg-blue-300 dark:bg-blue-800 rounded-md p-4 h-fit transition-all duration-500 px-4 py-4 w-1/2'>
-                    <input autoComplete='off' autoCorrect='false' type="text" name="title" id="titke" placeholder='New Folder Title...' className='rounded-full px-6 py-4 text-2xl text-gray-600 w-full hover:drop-shadow-lg active:drop-shadow-lg transition-all duration-300' value={controlledComponent.folder} onChange={(e) => updateControlledComponent({newValue: e.currentTarget.value, item: 'folder'})}/>
+                    <input autoComplete='off' autoCorrect='false' type="text" name="title" id="titke" placeholder='New Folder Title...' className='rounded-full px-6 py-4 text-2xl text-gray-600 w-full hover:drop-shadow-lg active:drop-shadow-lg transition-all duration-300' value={folderComponent.folder} onChange={(e) => updateFolderComponent({newValue: e.currentTarget.value, item: 'folder'})}/>
                     <section className='mt-2 flex flex-row items-center justify-start gap-2 ml-4'>
                         <p className='text-xl font-medium text-slate-200'>Color: </p>
-                        <input type="color" name="folder-color" className='cursor-pointer' id="folder-color" value={controlledComponent.color} onChange={(e) => updateControlledComponent({newValue: e.currentTarget.value, item: 'color'})} />
+                        <input type="color" name="folder-color" className='cursor-pointer' id="folder-color" value={folderComponent.color} onChange={(e) => updateFolderComponent({newValue: e.currentTarget.value, item: 'color'})} />
                     </section>
                 </form>
             </div>
 
             <div ref={ItemElement} className='overlay grid place-content-center duration-300' onClick={hideOverlay}>
-                <form action="" method="post">
+                <form action="" onSubmit={newItem} method="post" className='flex flex-col gap-4'>
                     <legend>Create Item</legend>
-                    <input type="text" placeholder='Name' />
-                    <Select options={folderList} />
+                    <input value={itemComponent.name} onChange={(e) => updateItemComponent({newValue: e.currentTarget.value, item: 'name'})} type="text" placeholder='Name' />
+                    <textarea value={itemComponent.note} onChange={(e) => updateItemComponent({newValue: e.currentTarget.value, item: 'note'})} name="note" cols="30" rows="10"></textarea>
+                    <Select onChange={(e) => updateItemComponent({newValue: e, item: 'folder'})} options={folderList} />
+                    <Select onChange={(e) => updateItemComponent({newValue: e, item: 'mode'})} options={modeList} />
+                    <button type="submit">Submit</button>
                 </form>
                 {/* onChange={(e) => handleChange(e)}  */}
                 {/* defaultValue={folderList[0]} */}
