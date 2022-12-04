d3e1418e777659a183b02061312c78c4d344f422
Benedict <benedict.arowo@gmail.com>
2022-12-04 11:27:12 +0300

    Added Error handling

--- front-end/src/fetchReq.js
+++ front-end/src/fetchReq.js
@@ -1,32 +1,42 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { API_URL } from "./config";
+import { useSetErr, useSetLoading } from "./contexts/GlobalContext";
 
-
-const FetchReq = createContext()
-export const useFetch = () => useContext(FetchReq)
+const FetchReq = createContext();
+export const useFetch = () => useContext(FetchReq);
 
 const FetchContext = ({ children }) => {
-	const Navigate = useNavigate()
+	const setLoading = useSetLoading();
+	const setErr = useSetErr();
+	const Navigate = useNavigate();
 	const fetchData = async ({ url, options }) => {
 		try {
+			setLoading(true);
 			const response = await fetch(`${API_URL}${url}`, options);
 			if (response.status === 401) {
 				// If user is not authenticated || Invalid credentials
-				localStorage.removeItem('token');
-				return Navigate("../login")
+				localStorage.removeItem("token");
+				return Navigate("../login");
+			}
+			if (!response.ok) {
+				throw new Error();
 			}
 			return response;
 		} catch (error) {
-			return error; // TODO: Handle error using error context
+			setErr({
+				status: true,
+				msg: "Error fetching data...",
+				solution: "Kindly please refresh the page.",
+			});
+		} finally {
+			setTimeout(() => {
+				setLoading(false);
+			}, 500); // To prevent loading screen flashing when 2 requests are made one after the other.
 		}
 	};
 
-	return (
-		<FetchReq.Provider value={fetchData}>
-			{children}
-		</FetchReq.Provider>
-	)
-}
+	return <FetchReq.Provider value={fetchData}>{children}</FetchReq.Provider>;
+};
 
-export default FetchContext
+export default FetchContext;
