14780978142bc845f06d49de08db1649857996ac
Benedict <benedict.arowo@gmail.com>
2022-12-04 11:25:22 +0300

    Prettier Formatting

--- front-end/src/components/Container.jsx
+++ front-end/src/components/Container.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const Container = () => {
-    return (
-        <div className='flex flex-row w-full h-full'>
-            <Outlet />
-        </div>
-    )
-}
+	return (
+		<div className="flex flex-row w-full h-full">
+			<Outlet />
+		</div>
+	);
+};
 
-export default Container
+export default Container;
